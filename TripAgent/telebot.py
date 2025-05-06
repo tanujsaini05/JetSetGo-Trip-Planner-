2a69c3b65e27063a7816db9dc1f0f3a740cdf7c1
Tanuj <sainisab264319@gmail.com>
2025-05-06 17:20:50 +0530

    Reflex based agent

---
+++ TripAgent/telebot.py
@@ -0,0 +1,23 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+TOKEN = "7820061278:AAF-MGiBNEthO0nIlJ2fCGETHLr2zuioG3U"
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("Hello! I'm your bot.")
+
+def echo(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(update.message.text)
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
